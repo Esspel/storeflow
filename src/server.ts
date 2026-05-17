12314415aa2ba66a475f3c48d980edad88bbad3f
Esspel <47383506+Esspel@users.noreply.github.com>
2026-05-17 13:38:55 +0200

    Fix server handler configuration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
-import { createRouter } from "./router";
 
-const handler = createStartHandler({ createRouter })(defaultStreamHandler);
-export default { fetch: handler };
+const fetch = createStartHandler(defaultStreamHandler);
+export default { fetch };
